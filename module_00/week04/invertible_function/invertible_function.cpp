2f05f1c87cdec2407e5cc2b22e4b2d96417bb489
FrenkenFlores <fflores@student.21-school.ru>
2021-09-04 09:56:32 +0300

    update #22: completed invertible_function task

---
+++ module_00/week04/invertible_function/invertible_function.cpp
@@ -0,0 +1,50 @@
+#include <vector>
+#include <algorithm>
+
+class FunctionPart {
+public:
+	FunctionPart(const char opr, const double  val) {
+		this->operation = opr;
+		this->value = val;
+	}
+	double Apply(double val) const{
+		if (operation == '+') {
+			val += value;
+		} else {
+			val -= value;
+		}
+		return val;
+	};
+	void Invert(void) {
+		if (operation == '+') {
+			operation = '-';
+		} else {
+			operation = '+';
+		}
+	}
+private:
+	char operation;
+	double value;
+};
+
+class Function {
+public:
+	void AddPart(const char opr, const double val) {
+		parts.push_back(FunctionPart(opr, val));
+	}
+	double Apply(double val) const {
+		for (const FunctionPart &part : parts) {
+			val = part.Apply(val);
+		}
+		return val;
+	}
+	void Invert(void) {
+		for (FunctionPart &part : parts) {
+			part.Invert();
+		}
+		std::reverse(begin(parts), end(parts));
+	}
+
+private:
+	std::vector<FunctionPart> parts;
+};
