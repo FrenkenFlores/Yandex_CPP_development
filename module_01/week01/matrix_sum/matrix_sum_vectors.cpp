17d01a4a67ef39de0d8da4c0a0632467993aedd9
FrenkenFlores <saifualdin.evloev@gmail.com>
2022-01-15 12:11:16 +0300

    update #41: week01/temparature

---
+++ module_01/week01/matrix_sum/matrix_sum_vectors.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+
+using namespace std;
+
+class Matrix {
+private:
+	int _num_rows;
+	int _num_columns;
+
+	vector<vector<int>> _matrix;
+
+public:
+	Matrix() {
+		_num_rows = 0;
+		_num_columns = 0;
+	}
+
+	Matrix(int num_rows, int num_columns) {
+		Reset(num_rows, num_columns);
+	}
+
+	void Reset(int num_rows, int num_columns) {
+		if (num_rows < 0) {
+			throw out_of_range("num_rows must be >= 0");
+		}
+		if (num_columns < 0) {
+			throw out_of_range("num_columns must be >= 0");
+		}
+		if (num_rows == 0 || num_columns == 0) {
+			num_rows = num_columns = 0;
+		}
+
+		_num_rows = num_rows;
+		_num_columns = num_columns;
+		_matrix.assign(num_rows, vector<int>(num_columns));
+	}
+
+	int& At(int row, int column) {
+		return _matrix.at(row).at(column);
+	}
+
+	int At(int row, int column) const {
+		return _matrix.at(row).at(column);
+	}
+
+	int GetNumRows() const {
+		return _num_rows;
+	}
+
+	int GetNumColumns() const {
+		return _num_columns;
+	}
+};
+
+bool operator==(const Matrix& one, const Matrix& two) {
+	if (one.GetNumRows() != two.GetNumRows()) {
+		return false;
+	}
+
+	if (one.GetNumColumns() != two.GetNumColumns()) {
+		return false;
+	}
+
+	for (int row = 0; row < one.GetNumRows(); ++row) {
+		for (int column = 0; column < one.GetNumColumns(); ++column) {
+			if (one.At(row, column) != two.At(row, column)) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+Matrix operator+(const Matrix& one, const Matrix& two) {
+	if (one.GetNumRows() != two.GetNumRows()) {
+		throw invalid_argument("Mismatched number of rows");
+	}
+
+	if (one.GetNumColumns() != two.GetNumColumns()) {
+		throw invalid_argument("Mismatched number of columns");
+	}
+
+	Matrix result(one.GetNumRows(), one.GetNumColumns());
+	for (int row = 0; row < result.GetNumRows(); ++row) {
+		for (int column = 0; column < result.GetNumColumns(); ++column) {
+			result.At(row, column) = one.At(row, column) + two.At(row, column);
+		}
+	}
+
+	return result;
+}
+
+istream& operator>>(istream& in, Matrix& matrix) {
+	int num_rows, num_columns;
+	in >> num_rows >> num_columns;
+
+	matrix.Reset(num_rows, num_columns);
+	for (int row = 0; row < num_rows; ++row) {
+		for (int column = 0; column < num_columns; ++column) {
+			in >> matrix.At(row, column);
+		}
+	}
+
+	return in;
+}
+
+ostream& operator<<(ostream& out, const Matrix& matrix) {
+	out << matrix.GetNumRows() << " " << matrix.GetNumColumns() << endl;
+	for (int row = 0; row < matrix.GetNumRows(); ++row) {
+		for (int column = 0; column < matrix.GetNumColumns(); ++column) {
+			if (column > 0) {
+				out << " ";
+			}
+			out << matrix.At(row, column);
+		}
+		out << endl;
+	}
+
+	return out;
+}
