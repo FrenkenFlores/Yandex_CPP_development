c3d13e4d1ee4051dd0c0e940710c8a621f673421
FrenkenFlores <saifualdin.evloev@gmail.com>
2022-01-15 11:59:12 +0300

    update #40: working on week01/matrix_sum

--- module_01/week01/matrix_sum/main.cpp
+++ module_01/week01/matrix_sum/main.cpp
@@ -1,21 +1,41 @@
 //
 // Created by FrenkenFlores on 13.01.2022.
 //
+
+// Fail some tests, should be implemented by STL vectors in the task
 #include <iostream>
-#include <fstream>
 #include <stdexcept>
-#include <vector>
 #include "matrix_sum.cpp"
 
 using namespace std;
+
 int main() {
 	Matrix one;
-//	Matrix two;
+	Matrix two;
 
+	cin >> one >> two;
+	cout << one + two << endl;
 
-	cin >> one;
-	cout << one;
-//	cin >> one >> two;
-//	cout << one + two << endl;
+	try {
+		std::cout << Matrix(0,5) + Matrix(8, 0);
+	} catch (exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	try {
+		std::cout << Matrix(-1,-1);
+	} catch (exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	try {
+		std::cout << Matrix(5,5).At(-1,-1);
+	} catch (exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	try {
+		std::cout << Matrix(5,5).At(5,5);
+	} catch (exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+
 	return 0;
 }
--- module_01/week01/matrix_sum/matrix_sum.cpp
+++ module_01/week01/matrix_sum/matrix_sum.cpp
@@ -1,13 +1,9 @@
 //
 // Created by FrenkenFlores on 13.01.2022.
 //
-
-#ifndef MATRIX_SUM
-#define MATRIX_SUM
-
 #include <new>
 #include <iostream>
-#include <iostream>
+#include <stdexcept>
 
 class Matrix {
 private:
@@ -19,32 +15,32 @@ public:
 	Matrix(): _num_rows(0), _num_cols(0) {
 		_matrix = nullptr;
 	}
-	Matrix(const int num_rows, const int num_cols): _num_rows(num_rows), _num_cols(num_cols) {
-		if (_num_rows < 0 || _num_cols < 0) throw std::out_of_range("Negative number of columns or rows");
-		try {
-			_matrix = new int*[_num_cols];
-			for (int i = 0; i < _num_cols; i++) {
-				_matrix[i] = new int[_num_rows];
-				for (int j = 0; j < num_rows; j++) {
-					_matrix[i][j] = 0;
-				}
-			}
-		} catch (const std::bad_array_new_length &e) {
-			std::cerr << e.what() << std::endl;
-		}
+	Matrix(const int num_rows, const int num_cols): _num_rows(num_rows), _num_cols(num_cols), _matrix(nullptr) {
+		Reset(_num_rows, _num_cols);
 	}
 
-	void Print() {
+	void Print() const{
 		for (int j = 0; j < _num_rows; j++) {
 			for (int i = 0; i < _num_cols; i++) {
-				std::cout << _matrix[j][i];
+				std::cout << &_matrix[j][i] << "(" << _matrix[j][i] << ")";
+				if (i != GetNumColumns() - 1)
+					std::cout << " ";
 			}
 			std::cout << std::endl;
 		}
 	}
-	void Rest(const int new_num_rows, const int new_num_cols) {
+
+	void Reset(const int new_num_rows, const int new_num_cols) {
+		_num_rows = new_num_rows;
+		_num_cols = new_num_cols;
+		if (_num_rows < 0) {
+			throw std::out_of_range("num_rows must be >= 0");
+		}
+		if (_num_cols < 0) {
+			throw std::out_of_range("num_columns must be >= 0");
+		}
+
 		// Clean old matrix
-		// TODO: segfault
 		if (_matrix) {
 			for (int i = 0; i < _num_cols; i++) {
 				delete[] _matrix[i];
@@ -53,21 +49,31 @@ public:
 		}
 
 		// If it is empty matrix
-		if (new_num_rows == 0 && new_num_cols == 0) {
+		if (_num_rows == 0 && _num_cols == 0) {
 			_matrix = nullptr;
 		}
 		else {
-			*this = Matrix(new_num_rows, new_num_cols);
+			try {
+				_matrix = new int*[_num_cols];
+				for (int i = 0; i < _num_cols; i++) {
+					_matrix[i] = new int[_num_rows];
+					for (int j = 0; j < _num_rows; j++) {
+						_matrix[i][j] = 0;
+					}
+				}
+			} catch (const std::bad_array_new_length &e) {
+				std::cerr << e.what() << std::endl;
+			}
 		}
 	}
 
 	int At(const int num_row, const int num_col) const {
-		if (num_row >= _num_rows || num_col >= _num_cols) throw std::out_of_range("Out of range");
+		if (num_row >= _num_rows || num_col >= _num_cols || num_row < 0 || num_col < 0) throw std::out_of_range("Out of range");
 		return _matrix[num_row][num_col];
 	}
 
 	int &At(const int num_row, const int num_col) {
-		if (num_row >= _num_rows || num_col >= _num_cols) throw std::out_of_range("Out of range");
+		if (num_row >= _num_rows || num_col >= _num_cols || num_row < 0 || num_col < 0) throw std::out_of_range("Out of range");
 		return _matrix[num_row][num_col];
 	}
 
@@ -80,7 +86,7 @@ public:
 	}
 
 	~Matrix() {
-		Rest(0, 0);
+		Reset(0, 0);
 	}
 
 };
@@ -88,16 +94,17 @@ public:
 std::istream &operator>>(std::istream &is, Matrix &matrix) {
 	int num_rows, num_cols;
 	is >> num_rows >> num_cols;
-	matrix.Rest(num_rows, num_cols);
+	matrix.Reset(num_rows, num_cols);
 	for (int j = 0; j < matrix.GetNumRows(); j++) {
 		for (int i = 0; i < matrix.GetNumColumns(); i++) {
 			is >> matrix.At(j, i);
 		}
 	}
+
 	return is;
 }
 
-std::ostream &operator<<(std::ostream &os, Matrix &matrix) {
+std::ostream &operator<<(std::ostream &os, const Matrix &matrix) {
 	os << matrix.GetNumRows() << " " << matrix.GetNumColumns() << std::endl;
 	for (int j = 0; j < matrix.GetNumRows(); j++) {
 		for (int i = 0; i < matrix.GetNumColumns(); i++) {
@@ -123,11 +130,17 @@ bool operator==(const Matrix &matrix1, const Matrix &matrix2) {
 	return true;
 }
 
-Matrix operator+(Matrix &matrix1, Matrix &matrix2) {
-	if (matrix1.GetNumColumns() != matrix2.GetNumColumns() || matrix1.GetNumRows() != matrix2.GetNumRows()) {
-		throw std::invalid_argument("Matrix have differ in size");
+Matrix operator+(const Matrix &matrix1, const Matrix &matrix2) {
+	if (matrix1.GetNumRows() != matrix2.GetNumRows()) {
+		throw std::invalid_argument("Mismatched number of rows");
 	}
+
+	if (matrix1.GetNumColumns() != matrix2.GetNumColumns()) {
+		throw std::invalid_argument("Mismatched number of columns");
+	}
+
 	Matrix new_matrix(matrix1.GetNumRows(), matrix1.GetNumColumns());
+
 	for (int j = 0; j < matrix1.GetNumRows(); j++) {
 		for (int i = 0; i < matrix1.GetNumColumns(); i++) {
 			new_matrix.At(j, i) = matrix1.At(j, i) + matrix2.At(j, i);
@@ -135,5 +148,3 @@ Matrix operator+(Matrix &matrix1, Matrix &matrix2) {
 	}
 	return new_matrix;
 }
-
-#endif
